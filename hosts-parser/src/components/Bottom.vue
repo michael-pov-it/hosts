e854b8e9ba453e52b0b8ff50b71cb3ef0c0fea1f
Mike G <michael.pov.it@gmail.com>
2020-08-28 01:55:25 +0300

    init commit

---
+++ hosts-parser/src/components/Bottom.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+
+  <section class="bottom">
+    <pre class='copy'>Copyright by {{ initials }}</pre>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'Bottom',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        initials: "M.G."
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="less">
+.copy {
+  width: 99%;
+  position: absolute;
+  bottom: 2em;
+  text-align: center;
+}
+</style>
---
+++ hosts-parser/src/views/Hosts.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hosts">
+    <HelloWorld msg="Welcome to Hosts page" />
+    <div id="hosts-list" v-for="host in this.hosts" :key="host.infrastructure">
+      <h2>Inftastructure</h2>
+      {{ host.infrastructure }}
+      <h2>vSphere</h2>
+      {{ host.vsphere }}
+      <h2>Inftastructure</h2>
+      {{ host.infrastructure }}
+    </div>
+    <Bottom />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue"
+import Bottom from "@/components/Bottom.vue"
+//var hosts = JSON.parse("@/assets/json/data.json")
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    Bottom
+  },
+  el() {
+    `#hosts-list`
+  },
+  data() {
+    return {
+      hosts: {
+        "telvent" : {
+          "infrastructure" : {
+            "server1": "10.10.11.12",
+            "server2": "13.14.15.16"
+          },
+          "vsphere": {
+            "server11": "17.18.19.20",
+            "server2222": "22.23.24.99"
+          }
+        }
+      }
+    }
+  }
+}
+</script>
